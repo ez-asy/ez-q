2a9ec1e2f4f914607acebf0d60277b2d1cffc8c3
JamesPatrickGill <jpgilldev@gmail.com>
2022-05-18 21:37:16 +0100

    feat: use env for redis url

--- src/redis_utils/redis_pool.rs
+++ src/redis_utils/redis_pool.rs
@@ -1,9 +1,12 @@
+use std::env;
+
 use anyhow::Result;
 use fred::pool::RedisPool;
 use fred::prelude::*;
 
 pub async fn create_redis_pool() -> Result<RedisPool> {
-    let config = RedisConfig::default();
+    let redis_url_env = env::var("REDIS_URL").unwrap_or(String::from("redis://localhost:6379"));
+    let config = RedisConfig::from_url(&redis_url_env).unwrap_or(RedisConfig::default());
     let policy = ReconnectPolicy::default();
     let pool = RedisPool::new(config, 6)?;
     let _ = pool.connect(Some(policy));
